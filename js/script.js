75f12b5d597b6bfde32f1fb89331e7d6b6fc0dff
Javierhu89 <javierpahissa@gmail.com>
2020-12-07 17:06:19 +0100

    index finalizada

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("asustado").addEventListener("click", function(){
+    document.getElementById("resplandor").style.visibility = "visible";
+})
+
+document.getElementById("valiente").addEventListener("mouseover", function(){
+    document.getElementById("valiente").style.backgroundColor = "#F5DEB3";
+})
+document.getElementById("asustado").addEventListener("mouseover", function(){
+    document.getElementById("asustado").style.backgroundColor = "#F5DEB3";
+})
