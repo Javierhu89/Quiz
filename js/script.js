2f1dda883153004d0d35a5b4a28aa48e60afa51f
Javierhu89 <javierpahissa@gmail.com>
2020-12-07 21:52:44 +0100

    Finalizado salvo medias query

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
-document.getElementById("asustado").addEventListener("click", function(){
-    document.getElementById("resplandor").style.visibility = "visible";
-})
+const respuestas = ["groenlandia", "edson" , "atenea" , "1986" , "recreativo", "matt", "amazonas" , "huelva", "renacido", "brendan"];
 
-document.getElementById("valiente").addEventListener("mouseover", function(){
-    document.getElementById("valiente").style.backgroundColor = "#F5DEB3";
-})
-document.getElementById("asustado").addEventListener("mouseover", function(){
-    document.getElementById("asustado").style.backgroundColor = "#F5DEB3";
-})
+document.getElementById("final").addEventListener("click",function(event){
+    event.preventDefault();
+    let aciertos=0;
+    let marcas=0;
+    let j = 0;
+    let answers = document.getElementsByName("preguntas");
+    let fondos =  document.getElementsByName("opciones");
+        for (let p=0 ; p<answers.length ; p++ ){
+            if (answers[p].checked){
+                marcas++;
+            }
+        }
+        if ( marcas < 10){
+            alert ("Responde a todas las preguntas, melón!");
+        } else {
+            for (let i=0 ; i<answers.length ; i++){
+                if (answers[i].checked){
+                    if (respuestas[j] == answers[i].value)
+                    {
+                    answers[i].parentElement.style.backgroundColor = "#228B22";
+                    answers[i].parentElement.style.border = "solid black";
+                    fondos[i].style.backgroundColor = "#228B22";
+                    aciertos++;
+                    }else{
+                        answers[i].parentElement.style.backgroundColor = "#B22222";
+                        answers[i].parentElement.style.border = "solid black";
+                        fondos[i].style.backgroundColor = "#B22222";
+                    } j++;
+                }
+            } alert ("Has acertado: " + aciertos);
+        }
+        if (aciertos == 10){
+            alert ("¡¿Cuántas veces lo has intentado para sacarlo?!");
+            alert ("Venga no te autoengañes, has mirado en google y lo sabes!");
+            alert ("Está bien, aquí tienes a Tu princesa:");
+            let princesa = document.createElement("img");
+            princesa.setAttribute("src","./img/princess.jpg");
+            princesa.setAttribute("id","princesa");
+            document.getElementById("_princesa").appendChild(princesa);
+        }
+});
