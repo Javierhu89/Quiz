fa70544a1a78548b34162e7552f2e377cdc36a04
Javierhu89 <javierpahissa@gmail.com>
2020-12-08 15:00:19 +0100

    Aumentar tamaño borde

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ document.getElementById("final").addEventListener("click",function(event){
                     if (respuestas[j] == answers[i].value)
                     {
                     answers[i].parentElement.style.backgroundColor = "#228B22";
-                    answers[i].parentElement.style.border = "solid black";
+                    answers[i].parentElement.style.border = "solid black 4px";
                     fondos[i].style.backgroundColor = "#228B22";
                     aciertos++;
                     }else{
                         answers[i].parentElement.style.backgroundColor = "#B22222";
-                        answers[i].parentElement.style.border = "solid black";
+                        answers[i].parentElement.style.border = "solid black 4px";
                         fondos[i].style.backgroundColor = "#B22222";
                     } j++;
                 }
